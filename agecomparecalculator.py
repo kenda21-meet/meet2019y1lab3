5f704e2538db58aa49a62806a8fdf084ce09e424
kenda21-meet <kenda21@meet.mit.edu>
2019-07-24 12:53:31 -0400

    Stopping here

---
+++ agecomparecalculator.py
@@ -0,0 +1,4 @@
+person_one=input("what is the age of the first person?")
+person_two=input("what is the age of the second person?")
+print(int(person_one)-int(person_two))
+print(int(person_one)==int(person_two))
